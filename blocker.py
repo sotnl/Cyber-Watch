fe54b7a9ba4cec39c51f3cae8fbcaf21b34b9ca3
curves <torreskirby5@gmail.com>
2026-05-11 20:18:53 +0800

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -1,25 +1,27 @@
 from database import get_connection
+from datetime import datetime, timedelta
 
+philippines_time = datetime.utcnow() + timedelta(hours=8)
 
-def block_ip(ip, reason="Brute force detected"):
+def block_device(device_id, reason="Brute force detected"):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT INTO blocked_ips (ip, reason)
-        VALUES (%s, %s)
-        ON CONFLICT (ip) DO UPDATE SET reason=%s
-    """, (ip, reason, reason))
+        INSERT INTO blocked_devices (device_id, reason, blocked_at)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (device_id) DO UPDATE SET reason=%s
+    """, (device_id, reason, philippines_time, reason, ))
 
     conn.commit()
     conn.close()
 
 
-def is_blocked(ip):
+def is_blocked(device_id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT 1 FROM blocked_ips WHERE ip=%s", (ip,))
+    cursor.execute("SELECT 1 FROM blocked_devices WHERE device_id=%s", (device_id,))
     result = cursor.fetchone()
 
     conn.close()
