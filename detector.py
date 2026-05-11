dbc68e5a637791538a1614e30d8e281a38ea1b04
curves <torreskirby5@gmail.com>
2026-05-11 20:19:44 +0800

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,37 +1,39 @@
 from database import get_connection
+from datetime import datetime, timedelta
 
 MAX_ATTEMPTS = 5
 
+philippines_time = datetime.utcnow() + timedelta(hours=8)
 
-def register_failed_attempt(ip):
+def register_failed_attempt(device_id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT count FROM failed_attempts WHERE ip=%s", (ip,))
+    cursor.execute("SELECT count FROM failed_attempts WHERE device_id=%s", (device_id,))
     row = cursor.fetchone()
 
     if row:
         cursor.execute("""
             UPDATE failed_attempts
             SET count = count + 1,
-                last_attempt = CURRENT_TIMESTAMP
-            WHERE ip = %s
-        """, (ip,))
+                last_attempt = %s
+            WHERE device_id = %s
+        """, (philippines_time, device_id,))
     else:
         cursor.execute("""
-            INSERT INTO failed_attempts (ip, count, last_attempt)
-            VALUES (%s, 1, CURRENT_TIMESTAMP)
-        """, (ip,))
+            INSERT INTO failed_attempts (device_id, count, last_attempt)
+            VALUES (%s, 1, %s)
+        """, (device_id, philippines_time,))
 
     conn.commit()
     conn.close()
 
 
-def detect_attack(ip):
+def detect_attack(device_id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT count FROM failed_attempts WHERE ip=%s", (ip,))
+    cursor.execute("SELECT count FROM failed_attempts WHERE device_id=%s", (device_id,))
     row = cursor.fetchone()
 
     conn.close()
@@ -42,13 +44,11 @@ def detect_attack(ip):
     return row[0] >= MAX_ATTEMPTS
 
 
-def clear_failed_attempts(ip):
+def clear_failed_attempts(device_id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("""
-        DELETE FROM failed_attempts WHERE ip=%s
-    """, (ip,))
+    cursor.execute("DELETE FROM failed_attempts WHERE device_id=%s", (device_id,))
 
     conn.commit()
     conn.close()
